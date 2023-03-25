79fc928e7d5104f2746ba9e82b742d6fa7499435
fanjunhong <fanjunhong.qh@crrcgc.cc>
2023-03-25 23:05:34 +0800

    second commit

--- src/com/qrrs/xxb/test/HelloGit.java
+++ src/com/qrrs/xxb/test/HelloGit.java
@@ -3,5 +3,6 @@ package com.qrrs.xxb.test;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("This is My first GitDemo....");
+        System.out.println("Add Git test....");
     }
 }
