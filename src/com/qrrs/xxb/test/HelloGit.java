c9e395fa9eaf4778c2645fe733102d6f944996d4
fanjunhong <fanjunhong.qh@crrcgc.cc>
2023-03-25 23:02:23 +0800

    first commit

---
+++ src/com/qrrs/xxb/test/HelloGit.java
@@ -0,0 +1,7 @@
+package com.qrrs.xxb.test;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("This is My first GitDemo....");
+    }
+}
---
+++ src/com/qrrs/xxb/vo/DeptVo.java
@@ -0,0 +1,31 @@
+package com.qrrs.xxb.vo;
+
+public class DeptVo {
+    private String deptid;
+    private String deptcode;
+    private String deptname;
+
+    public String getDeptid() {
+        return deptid;
+    }
+
+    public void setDeptid(String deptid) {
+        this.deptid = deptid;
+    }
+
+    public String getDeptcode() {
+        return deptcode;
+    }
+
+    public void setDeptcode(String deptcode) {
+        this.deptcode = deptcode;
+    }
+
+    public String getDeptname() {
+        return deptname;
+    }
+
+    public void setDeptname(String deptname) {
+        this.deptname = deptname;
+    }
+}
